edffe67889741ef0e43e903848f63d0ff6bc03d7
Laura Lourenco Lucca <llourenc@everis.com>
2019-01-20 16:16:42 -0200

    utilizando diretivas para melhorar o código

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Raposinhas';
+  photos = [
+    {
+      url: 'https://www.criptomoedasfacil.com/wp-content/uploads/2018/11/74e53faa3d7e2fbc.jpg',
+      description: '"closeup red fox'
+    }, {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXlvonVjp7pE4EXXgLlPLzDlSnYveu3BU_KuNIgxN63zvT_WdkzA',
+      description: 'fennec fox'
+    }, {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9oRdPDIB1wl-ZCEs7xDBS4-vHM6MFuWItulZSOPkcMJbLi3iXPg',
+      description: 'arctic fox'
+    }
+  ];
 }
