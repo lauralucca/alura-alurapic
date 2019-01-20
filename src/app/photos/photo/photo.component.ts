c70b1bcb82061dff73ddb74004cdbb983c0be009
Laura Lourenco Lucca <llourenc@everis.com>
2019-01-20 15:24:12 -0200

    iniciando o projeto e criando o primeiro componente (ap-photo)

---
+++ src/app/photos/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-ap-photo',
+    templateUrl: 'photo.component.html',
+    styleUrls: ['photo.component.css']
+})
+export class PhotoComponent {
+    @Input() url = '';
+    @Input() description = '';
+}
